c0aadac85164553175789b806b96d4c770b1f160
emmanuelvp88 <emmanuel.villalvap@outloo.es>
2024-01-07 23:19:01 -0600

    set atribute

---
+++ 20-Atributos de inputs/index.js
@@ -0,0 +1,7 @@
+const coler = document.querySelector(".titulo");
+coler.setAttribute("contentEditable", "true");
+
+
+const input = document.querySelector(".input-normal");
+input.type = "range";
+input.type = "color";
