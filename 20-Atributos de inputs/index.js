1bdec5d168082d00e5b466735f0c1591fdd06415
emmanuelvp88 <emmanuel.villalvap@outloo.es>
2024-01-07 23:53:05 -0600

    querySelector

--- 20-Atributos de inputs/index.js
+++ 20-Atributos de inputs/index.js
@@ -1,7 +1,10 @@
 const coler = document.querySelector(".titulo");
 coler.setAttribute("contentEditable", "true");
 
-
 const input = document.querySelector(".input-normal");
 input.type = "range";
-input.type = "color";
+
+document.write(`1.- getElementById():  Selecciona un elemento por Id<br>
+2.-getElementsBytagName(): Selecciona todos los elemetos que coincidan con la etiqueta seleccionada.<br>
+3.- querySelector(): Devuelve el primer elemeto que conicida con el grupo espesificado de selectores.<br>
+4.- querySelectorAll(): Delvuelve todos os elementos que cincidan con el grupo espesificada de selectores.<br> `);
